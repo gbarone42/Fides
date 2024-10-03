f5551477eb7f9e78a9fcf15d6f8b85c28e478134
gbarone42 <badph3@gmail.com>
2024-10-03 16:20:36 +0200

    older_version

--- db-init/init.sql
+++ db-init/init.sql
@@ -5,13 +5,13 @@ CREATE TABLE IF NOT EXISTS users (
   role VARCHAR(50) NOT NULL -- 'business' o 'employee'
 );
 
--- Update activities table to include time and place columns
+--
 CREATE TABLE IF NOT EXISTS activities (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   description TEXT,
   date DATE NOT NULL,
-  time TIME NOT NULL,  -- New column for time
+  time TIME NOT NULL,  -- 
   place VARCHAR(255) NOT NULL,  -- New column for place
   user_id INTEGER REFERENCES users(id)
 );
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS activities (
 CREATE TABLE IF NOT EXISTS permissions (
   id SERIAL PRIMARY KEY,
   role VARCHAR(50) NOT NULL, -- 'business' o 'employee'
-  permission VARCHAR(255) NOT NULL -- es: 'manage_employees'
+  permission VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS availability (
